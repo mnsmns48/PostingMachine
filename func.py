086ec6ac7a496da4e0dcb2196a639061117b7cd4
mnsmns48 <getzboy@list.ru>
2024-06-18 08:32:48 +0300

    add in notification second admin alert

--- func.py
+++ func.py
@@ -261,12 +261,14 @@ async def scrape_vk_data(data: dict, session: ClientSession, **kwargs) -> dict:
 
 
 async def send_notification(session: ClientSession, text: str):
-    async with session.get(
-            url=f"https://api.telegram.org/bot{source_settings.bot_token.get_secret_value()}"
-                f"/sendMessage?chat_id={source_settings.post_editor}"
-                f"&text=Обновлено:{text}"
-    ) as resp:  # f"&disable_notification={source_settings.disable_notification}"
-        await resp.json()
+    for admin in source_settings.telegram_admin_id:
+        async with session.get(
+                url=f"https://api.telegram.org/bot{source_settings.bot_token.get_secret_value()}"
+                    f"/sendMessage?chat_id={admin}"
+                    f"&text=Обновлено:{text}"
+        ) as resp:  # f"&disable_notification={source_settings.disable_notification}"
+            await resp.json()
+        await asyncio.sleep(0.1)
 
 
 # async def alert_editor(source: str):
