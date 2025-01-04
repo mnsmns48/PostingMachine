a999e5078383f9110615ad049de7ed78f22a503a
mnsmns48 <getzboy@list.ru>
2025-01-04 10:48:38 +0300

    add Second admin

--- func.py
+++ func.py
@@ -263,14 +263,13 @@ async def scrape_vk_data(data: dict, session: ClientSession, **kwargs) -> dict:
 
 
 async def send_notification(session: ClientSession, text: str):
-    for admin in source_settings.telegram_admin_id[1:]:
+    for admin in source_settings.telegram_admin_id:
         async with session.get(
                 url=f"https://api.telegram.org/bot{source_settings.bot_token.get_secret_value()}"
                     f"/sendMessage?chat_id={admin}"
                     f"&text={await read_and_choose_random_line()} Женечка❤️ :{text}"
         ) as resp:  # f"&disable_notification={source_settings.disable_notification}"
             await resp.json()
-        # await asyncio.sleep(0.1)
 
 
 async def read_and_choose_random_line():
